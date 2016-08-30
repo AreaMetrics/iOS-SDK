d4b0e8f114a25cc7ca186038cbcd0b40cbf2efee
Ryo Tulman <ryo.tulman@gmail.com>
2016-08-29 20:16:58 -0700

    Framework updated to 2.4.0

--- AreaMetricsSDK.framework/Versions/A/Headers/AreaMetricsSDK.h
+++ AreaMetricsSDK.framework/Versions/A/Headers/AreaMetricsSDK.h
@@ -6,37 +6,37 @@
 //  Copyright © 2016 AreaMetrics. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
 #import <CoreBluetooth/CoreBluetooth.h>
 #import <AdSupport/AdSupport.h>
 
-#define AREAMETRICS_PROXIMITY_UUID [[NSUUID alloc] initWithUUIDString:@"B9407F30-F5F8-466E-AFF9-25556B577272"]
-
-//! Project version number for AreaMetricsSDK.
-FOUNDATION_EXPORT double AreaMetricsSDKVersionNumber;
-
-//! Project version string for AreaMetricsSDK.
-FOUNDATION_EXPORT const unsigned char AreaMetricsSDKVersionString[];
+// If setting up any beacons of your own, use the AreaMetricsSDK Proximity UUID "B9407F30-F5F8-466E-AFF9-25556B577272"
 
 @interface AreaMetricsSDK : NSObject
 
-@property (strong, nonatomic) NSString *appID;      // Required - APP ID from AreaMetrics
-@property (strong, nonatomic) NSString *apiKey;     // Required - API Key from AreaMetrics
-@property (copy, nonatomic) NSString *appVersion; // Optional - set your app version string
-@property (nonatomic, assign) bool setIDFA; // Optional - set IDFA (defaults to 'true' unless changed before 'startService' is called)
++(instancetype)sharedInstance;
 
-+(AreaMetricsSDK *) sharedInstance;
--(void) startService;               // Required - start the AreaMetrics service SDK
--(void) setUserAge:(int) value;     // Optional - set the user's age
--(void) setUserGender:(bool) value; // Optional - set user's gender (0 for female or 1 for male)
--(void) setUserZipCode:(NSString *) value;  // Optional - set user's zip code
--(void) setUserID:(NSString *) value;       // Optional - set user's id within your app
--(void) setUserData:(NSString *) category parameter:(NSString *) parameter value:(NSString *) value;    // Optional - set advanced user data (see documentation at areametrics.readme.io for more details)
+-(void)startServiceForAppID:(NSString *)appId withAPIKey:(NSString *)apiKey; //Required and must be the first call
 
--(NSString *) getVersionString;
--(NSString *) getUUID;
+-(void)setUserAge:(NSInteger)age; // Optional - set the user's age
+-(void)setUserGender:(BOOL)gender; // Optional - set user's gender (0 for female or 1 for male)
+-(void)setUserZipCode:(NSString *)zip; // Optional - set user's zip code
+-(void)setUserID:(NSString *)userId; // Optional - set user's id within your app
+-(void)setCustomDataForKey:(NSString *)key withValue:(NSString *)value; //Optional - set custom data
 
-//preLocationInitialize is deprecated! Do not call it, and please remove old code calling it. It is no longer required and does not have any functionality. Simply retained as an empty function as a courtesy for updaters.
+// Deprecated! Instead, use startServiceForAppID:withAPIKey:
+@property (copy, nonatomic) NSString *appID __deprecated;
+@property (copy, nonatomic) NSString *apiKey __deprecated;
+-(void)startService __deprecated;
+
+//Deprecated! Instead, drop the category and use setCustomDataForKey:withValue:
+-(void)setUserData:(NSString *)category parameter:(NSString *)parameter value:(NSString *)value __deprecated;
+
+//Deprecated! These can be totally removed
+@property (copy, nonatomic) NSString *appVersion __deprecated;
+@property (nonatomic, assign) BOOL setIDFA __deprecated;
 -(void)preLocationInitialize __deprecated;
 
 @end
