aeefa976f67a178d7b9ad02f91f82936fcf25e80
Ryo Tulman <ryo.tulman@gmail.com>
2016-11-29 17:16:04 -0800

    Upgrade framework file and podspec for new release

--- AreaMetricsSDK.framework/Versions/A/Headers/AreaMetricsSDK.h
+++ AreaMetricsSDK.framework/Versions/A/Headers/AreaMetricsSDK.h
@@ -14,7 +14,15 @@
 
 // If setting up your own beacons, use AreaMetrics Proximity UUID "B9407F30-F5F8-466E-AFF9-25556B577272"
 
+@protocol AreaMetricsSDKDelegate <NSObject>
+//Implement this delegate to receive user persona
+@optional
+-(void)didGeneratePersona:(NSDictionary *)persona;
+@end
+
 @interface AreaMetricsSDK : NSObject
+
+@property (nonatomic, weak) id<AreaMetricsSDKDelegate> delegate;
 
 +(instancetype)sharedInstance;
 
@@ -25,18 +33,5 @@
 -(void)setUserZipCode:(NSString *)zip; // Optional - set user's zip code
 -(void)setUserID:(NSString *)userId; // Optional - set user's id within your app
 -(void)setCustomDataForKey:(NSString *)key withValue:(NSString *)value; //Optional - set custom data
-
-// Deprecated! Instead, use startServiceForAppID:withAPIKey:
-@property (copy, nonatomic) NSString *appID __deprecated;
-@property (copy, nonatomic) NSString *apiKey __deprecated;
--(void)startService __deprecated;
-
-//Deprecated! Instead, drop the category and use setCustomDataForKey:withValue:
--(void)setUserData:(NSString *)category parameter:(NSString *)parameter value:(NSString *)value __deprecated;
-
-//Deprecated! These can be totally removed
-@property (copy, nonatomic) NSString *appVersion __deprecated;
-@property (nonatomic, assign) BOOL setIDFA __deprecated;
--(void)preLocationInitialize __deprecated;
 
 @end
